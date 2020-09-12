6a34c0e9e4e0a420d9fe24f394e19a4e664f8a6c
Nourhan <nourhanjamalx@gmail.com>
2020-09-12 18:10:20 +0200

    created setup, run and teardown methods

---
+++ Tests.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def setUp(self):
+        pass
+
+    def run(self):
+        self.setUp()
+        method = getattr(self,self.name)
+        method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
+
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self,name)
+
+    def setUp(self):
+        self.log="setUp "
+
+    def testMethod(self):
+        self.log= self.log+"testMethod "
+
+    def tearDown(self):
+        self.log=self.log +"tearDown "
+
+
+
+class TestCaseTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod tearDown " == test.log)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
